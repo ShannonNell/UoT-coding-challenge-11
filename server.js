d37174a578b2de40dfcd0cf97c96aac56900e1fb
Shannon <shannon.nell92@gmail.com>
2021-06-28 10:54:39 -0400

    Server reads incoming data

--- server.js
+++ server.js
@@ -7,24 +7,40 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// //parse incoming string/array data
-// app.use(express.urlencoded({ extended: true }));
-// //parse incoming JSON data
-// app.use(express.json());
+// Used to accept incoming data so we can use it (insomnia tested)
+//parse incoming string/array data 
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 // app.use(express.static('public'));
 
-//create new note
+//create new note and add to json file
+function createNewNote(body, notesArr) {
+    console.log(body);
+    const note = body;
+    notesArr.push(note);
 
+    fs.writeFileSync(
+        path.join(__dirname, './')
+    )
+}
 //validate notes
 
-//get saved notes
-app.get('/api/notes', (req, res) => {    
-    res.json(notes);
-});
 
 //delete note
 
+//get saved notes
+app.get('/api/notes', (req, res) => { 
+    res.json(notes);
+});
 
+//post notes
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+//HTTP ROUTES
 //get notes html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
